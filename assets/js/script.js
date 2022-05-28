2c8b9a1218c5c5af3c1a40fa8e23b4a66382f1b6
Angie <103882157+anakela@users.noreply.github.com>
2022-05-28 13:34:12 -1000

    Updated JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,25 @@
 // Fill the "currentDay" p tag with the local time zone's date.
 $("#currentDay").text(new Date().toLocaleDateString());
 
-// Use for loop to go through the hour divs and compare to current local time to change the colors.
-// for (let i = 0; i < ; i++) {
-//     const element = array[i];
-
-// }
-
+// Set a for loop to review each textarea with class "description" and change colors accordingly.
 $(".description").each(function () {
     const today = new Date();
     const todaysHours = today.getHours();
+    console.log(todaysHours);
 
     for (let i = 8; i <= 17; i++) {
         // const element = array[i];
         if (i < todaysHours) {
-            $("#hour" +i).addClass(".present");
-        } else if (i > todaysHours) {
-            $("#hour" + i).addClass(".future");
+            $("#hour" + i).addClass("past");
+        } else if (i === todaysHours) {
+            $("#hour" + i).addClass("present");
         } else {
-            $("#hour").addClass(".past");
+            $("#hour" + i).addClass("future");
         }
     }
 });
 
-// $(".hour").each(function () {
-//     console.log($(this).text());
-//     console.log($("#currentDay").val);
-
-//     const today = new Date();
-//     const todaysHours = today.getHours();
-//     const todaysMinutes = today.getMinutes();
-
-//     console.log(todaysHours);
-//     console.log(todaysMinutes);
-
-//     console.log($(this).text.val);
-
-//     // Compare this to local time.
-//     if ($(this).text.val === todaysHours.val) {
-//         // Change class to .present. 
-//         $(".description").addClass(".present");
-//     } // else if ($(this).text === ) {
-//     //     // Change class to .past.
-//     //     $(".description").addClass(".past");
-//     // } else {
-//     //     // Change class to .future.
-//     //     $(".description").addClass(".future");
-//     // }
-// });
-
+// When the save button is clicked, store the data in localstorage.
 $(".saveBtn").on("click", function (storeEvent) {
     console.log(storeEvent);
     let keyName = $(this).siblings(".description").attr("id");
@@ -60,8 +31,10 @@ $(".saveBtn").on("click", function (storeEvent) {
     // if ($(".description").text !== '') {
     //     $(".description").text(localStorage.content);
     // }
-    localStorage.getItem();
-
+    if (keyName === $("#hour" + i)) {
+        localStorage.getItem(keyName, textValue);
+        $(".description").appendChild();
+    }
 
 });
 
